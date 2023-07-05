185b5a19d2757bfaf2a164511094feb6e14a8251
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-07-05 08:21:02 -0700

    wraps app in account provider

--- src/index.js
+++ src/index.js
@@ -10,14 +10,17 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { AccountProvider } from "./context/AccountContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <CssBaseline />
-            <App />
-        </BrowserRouter>
+        <AccountProvider>
+            <BrowserRouter>
+                <CssBaseline />
+                <App />
+            </BrowserRouter>
+        </AccountProvider>
     </React.StrictMode>
 );
 
