eaf2ba6586af6181f3a6614f29a6ebe7fd849037
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-07-06 18:10:40 -0700

    refactors site header

--- src/components/SiteHeader/SiteHeader.js
+++ src/components/SiteHeader/SiteHeader.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Box, IconButton, Typography } from "@mui/material";
-import LoginIcon from "@mui/icons-material/Login";
+import { Box, Paper, Typography } from "@mui/material";
 import "./SiteHeader.css";
 
 export default function SiteHeader() {
     return (
         <Box component={"div"} className="header-wrapper">
-            <Typography className={"site-header"} variant={"h1"}>
-                Jake's Tech Journal
-            </Typography>
-            <IconButton className={"login-icon"}>
-                <LoginIcon className={"login-icon"} />
-            </IconButton>
+            <Paper className={"header-paper"}>
+                <Typography className={"site-header"} variant={"h1"}>
+                    Jake's Tech Journal
+                </Typography>
+            </Paper>
         </Box>
     );
 }
